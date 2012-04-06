7b76b3d241850e3c8c54ab71a28e0b2006e66713
achang.zhou <achang.zhou@gmail.com>
2012-04-06 05:42:47 +0000

    Preparing for audio support.

--- droidipcam/jni/mediabuffer.cpp
+++ droidipcam/jni/mediabuffer.cpp
@@ -17,11 +17,13 @@ MediaBuffer::MediaBuffer(const unsigned int vpkg_number,
     vpkg_pool_.push_back(pkg);   
   }
   vpkg_released = new MediaPackage(vpkg_size_);
+  vpkg_seq_ = 0;
 
   for(unsigned int i = 0; i < apkg_number; i++) {
     pkg = new MediaPackage(apkg_size_);
     apkg_pool_.push_back(pkg);   
   }
+  apkg_released = new MediaPackage(apkg_size_);
 
 }
 
@@ -39,6 +41,9 @@ MediaBuffer::~MediaBuffer(){
   }
   apkg_pool_.clear();
 
+  delete vpkg_released;
+  delete apkg_released;
+
 }
 
 void MediaBuffer::Reset() {
@@ -50,6 +55,7 @@ void MediaBuffer::Reset() {
     vpkg_pool_.push_back(pkg);
   } 
   vbuffer_.clear();
+  vpkg_seq_ = 0;
 
   while(!abuffer_.empty()) {
     pkg = abuffer_.front();
@@ -57,14 +63,15 @@ void MediaBuffer::Reset() {
     apkg_pool_.push_back(pkg);
   } 
   abuffer_.clear();
+
+
 }
 
 bool MediaBuffer::pushVideoPackage(const unsigned char *d, const unsigned int len, const unsigned int ts, const unsigned int isIntra) {
-  static unsigned int global_seq_count = 0;
   MediaPackage *pkg = NULL;
   bool valid = false;
  
-  global_seq_count ++;
+  vpkg_seq_ ++;
 
   // 1. check memory space.
   if ( len > vpkg_size_ ) {
@@ -85,7 +92,7 @@ bool MediaBuffer::pushVideoPackage(const unsigned char *d, const unsigned int le
   if ( pkg == NULL) {
       valid = true;                                     // first frame
   } else {
-    if ( (pkg->seq + 1) == global_seq_count ){
+    if ( (pkg->seq + 1) == vpkg_seq_ ){
       valid = true;                                     // continued frame
     } else if ( isIntra ){
       valid = true;                                     // new intra frame
@@ -104,7 +111,7 @@ bool MediaBuffer::pushVideoPackage(const unsigned char *d, const unsigned int le
   }
   pkg->ts = ts;
   pkg->length = len;
-  pkg->seq = global_seq_count;
+  pkg->seq = vpkg_seq_;
   if ( isIntra )
     pkg->media_type = MEDIA_TYPE_VIDEO_KEYFRAME;
   else
@@ -138,6 +145,7 @@ bool MediaBuffer::pushAudioPackage(const unsigned char *d, const unsigned int le
 
   pkg->ts = ts;
   pkg->length = len;
+  pkg->seq = 0;               // We don't need sequece for audio
   pkg->media_type = MEDIA_TYPE_AUDIO;
   memcpy(pkg->data, d, len);
 
@@ -166,46 +174,44 @@ bool MediaBuffer::PushBuffer(const unsigned char *d, const unsigned int len,
 
 bool MediaBuffer::PullBuffer(MediaPackage **ppkg,const MEDIA_TYPE mt) {
   std::list<MediaPackage *> *pBuffer;
-
-  if ( mt == MEDIA_TYPE_AUDIO )
-    pBuffer = &abuffer_;
-  else
-    pBuffer = &vbuffer_;
-
-  if (pBuffer->size() == 0)
-      return false;
-  
-  MediaPackage *pkg = pBuffer->front();
-  
-  vpkg_released->ts = pkg->ts;
-  vpkg_released->length = pkg->length;
-  vpkg_released->media_type = pkg->media_type;
-  vpkg_released->seq = pkg->seq;
-  memcpy(vpkg_released->data, pkg->data, vpkg_released->length);
-  *ppkg = vpkg_released;
-
-  releaseBuffer(pkg);
-
-  return true;
-}
-
-void MediaBuffer::releaseBuffer(MediaPackage *pkg) {
-  std::list<MediaPackage *> *pBuffer;
   std::vector<MediaPackage *> *pPool;
+  MediaPackage *pkg_released;
 
-  if ( pkg->media_type == MEDIA_TYPE_AUDIO ) {
-    pBuffer = &abuffer_;
-    pPool = &apkg_pool_;
-  } else {
+  *ppkg = NULL;
+
+  if ( mt == MEDIA_TYPE_VIDEO ) {
     pBuffer = &vbuffer_;
     pPool = &vpkg_pool_;
+    pkg_released = vpkg_released;
+  } else {
+    pBuffer = &abuffer_;
+    pPool = &apkg_pool_;
+    pkg_released = apkg_released;
   }
+
+  MediaPackage *pkg; 
+  {
+    talk_base::CritScope lock(&mutex_);
+
+    if (pBuffer->size() == 0)
+        return false;
+  
+    pkg = pBuffer->front();
+    pBuffer->pop_front();
+  } 
+
+  pkg_released->seq = pkg->seq;  
+  pkg_released->ts = pkg->ts;
+  pkg_released->length = pkg->length;
+  pkg_released->media_type = mt;
+  memcpy(pkg_released->data, pkg->data, pkg_released->length);
+  *ppkg = pkg_released;
 
   { 
     talk_base::CritScope lock(&mutex_);
-    pBuffer->pop_front();
     pPool->push_back(pkg);
   }
-  return;
+
+  return true;
 }
 
