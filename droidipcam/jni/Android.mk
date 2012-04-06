dcb47c4550b0305e993fe9a4224569fdb8204644
achang.zhou <achang.zhou@gmail.com>
2012-04-06 06:25:21 +0000

    Added pre-build ffmpeg library.

--- droidipcam/jni/Android.mk
+++ droidipcam/jni/Android.mk
@@ -5,6 +5,15 @@
 LOCAL_PATH:= $(call my-dir)
 
 ###########################################################
+# the third prebuild library for linking
+#
+include $(CLEAR_VARS) 
+LOCAL_MODULE := libffmpeg-prebuild	
+LOCAL_SRC_FILES := libffmpeg.so
+LOCAL_MODULE_TAGS := prebuild
+include $(PREBUILT_SHARED_LIBRARY)
+
+###########################################################
 # build android libteaony 
 # libteaonly: clone from libjingle, for generat NDK appplicaton
 #      threads, socket, signal/slot and messages, etc.
@@ -28,7 +37,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := ipcamera
 LOCAL_CPPFLAGS := -O2 -Werror -Wall -DHAMMER_TIME=1 -DLOGGING=1 -DHASHNAMESPACE=__gnu_cxx -DHASH_NAMESPACE=__gnu_cxx -DPOSIX -DDISABLE_DYNAMIC_CAST -D_REENTRANT -DOS_LINUX=OS_LINUX -DLINUX -D_DEBUG  -DANDROID
 LOCAL_C_INCLUDES :=  ./
-LOCAL_SHARED_LIBRARIES := libteaonly
+LOCAL_SHARED_LIBRARIES := libffmpeg-prebuild libteaonly
 LOCAL_LDLIBS := -llog
 
 include $(LOCAL_PATH)/build.mk
